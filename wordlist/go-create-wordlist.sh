ee0be8ba2de04cfedd7fb31456534776cfac385e
Douglas Muth <dmuth@dmuth.org>
2016-07-10 19:04:23 -0400

    Added script to create the wordlist (automation is good!)

---
+++ wordlist/go-create-wordlist.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Wrapper to create our wordlist from any directory
+#
+
+# Errors fatal
+set -e
+
+pushd $(dirname $0) > /dev/null
+
+JS="wordlist.js"
+
+echo "# "
+echo "# Creating wordlist Javascript..."
+echo "# "
+
+
+./create-wordlist.php > ${JS}
+
+echo "# "
+echo "# Done!  List created at ${JS}"
+echo "# "
+
+
