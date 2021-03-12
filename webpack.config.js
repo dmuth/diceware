8cec58c05d9287a9958548658603e609666fea57
Douglas Muth <dmuth@dmuth.org>
2021-03-12 17:08:21 -0500

    Fix up for webpack 5.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@
 //
 var path = require('path');
 
+const webpack = require('webpack')
+
 //
 // Compile main.js (and its dependencies) into dist/bundle.js.
 //
@@ -12,6 +14,19 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  resolve: {
+    fallback: {
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "vm": require.resolve("vm-browserify")
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser.js'
+    })
+  ]
 };
 
