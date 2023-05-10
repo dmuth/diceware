dda4262bceeaa2407c933964f644c6bceff44852
Douglas Muth <dmuth@dmuth.org>
2023-05-09 21:26:46 -0400

    Added CloudFront invalidation to deployment script.

--- go-sync-to-s3.sh
+++ go-sync-to-s3.sh
@@ -8,6 +8,11 @@ set -e
 
 pushd $(dirname $0) > /dev/null
 
+echo "# Syncing files to AWS S3 bucket..."
 aws s3 sync . s3://diceware.dmuth.org/ --exclude ".*" --exclude "node_modules/*" --delete
 
+echo "# Invalidating CloudFront cache..."
+aws cloudfront create-invalidation --distribution-id E2TUL4ST85ZH5Y --paths "/*"
+
+echo "# Done!"
 
